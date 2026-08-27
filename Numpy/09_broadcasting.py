06760f467ced99e7cd36bc54bbca54bf5a4490c0
kanishkambk43 <kanishkambk43@gmail.com>
2026-08-27 18:31:50 +0530

    Brodcasting

--- Numpy/09_broadcasting.py
+++ Numpy/09_broadcasting.py
@@ -1,5 +1,7 @@
 # 09_broadcasting.py
+Broadcasting in NumPy basically means:
 
+NumPy automatically treats a smaller array/value as if it were repeated to match a bigger array, so the operation can happen element-by-element.
 import numpy as np
 
 
@@ -14,3 +16,18 @@ print("Array:", arr)
 
 print("Result:", result)
 # Output: Result: [15 25 35 45]
+
+#Brodcasting a column
+arr = np.array([
+    [10, 20, 30],
+    [40, 50, 60]
+])
+
+values = np.array([
+    [1],
+    [2]
+])
+
+result = arr + values
+
+print(result)
